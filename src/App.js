e37d11554fc061e91054afd5067c9acbea488e58
souza99 <jpfsouza99@gmail.com>
2022-10-14 00:10:20 -0300

    Criando comentarios no post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PostCreat from "./PostCreat";
+import PostList from "./PostList";
 
 const App = () => {
     return (
         <div className="container">
             <h1>Creat Post</h1>
             <PostCreat />
+            <hr />
+            <h1>PostList</h1>
+            <PostList />
         </div>
     )
 
